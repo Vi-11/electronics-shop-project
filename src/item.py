15c5f86a73d2087e6f140e65af4b377f3ba837a5
Jonh Smith <vi801414@gmail.com>
2024-10-12 19:38:41 +0300

    Update main.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+from bokeh.core.property.bases import Property
+from sipbuild.generator.parser.annotations import integer
+from tifffile import astype
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,11 +11,28 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self,name):
+        self.__name = name[0:11]
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        import csv
+        with open(file=r'E:\electronics-shop-project\src\items.csv',encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                a=cls(row['name'],row['price'],row['quantity'])
+    @staticmethod
+    def string_to_number(str):
+        return int(float(str))
 
     def calculate_total_price(self) -> float:
         """
